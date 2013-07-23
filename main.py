b989fc6cb737fcfa3d5c8c4751514e7e628d926b
DTailor <dann.croitoru@gmail.com>
2013-07-23 17:05:10 +0300

    adds PostaParser Class

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import urllib2
 QUERY_STRING = "http://www.posta.md:8081/IPSWeb_item_events.asp?itemid=%s&Submit=Accept"
 
 
-def query_string(code):
-	return QUERY_STRING % code
+class PostaParser(object):
+	"""
+	Tracking number parser to posta.md in
+	order to get the html and sanitize it 
+	for a human readable form
+	"""
+	def __init__(self, track_nr):
+		super(PostaParser, self).__init__()
+		self.track_nr = track_nr
+		self.query_string = QUERY_STRING % track_nr
 
-def read_link(link):
-	page = urllib2.urlopen(link)
-	data = page.read()
-	page.close()
-	return data
+	def get_html(self):
+		page = urllib2.urlopen(self.query_string)
+		data = page.read()
+		page.close()
+		return data
+		
+
